10e0a570130e3a513f5e0f97240ce26100cf4dba
SheepVand <66167164+SheepVand0@users.noreply.github.com>
2023-01-20 09:22:33 +0100

    Removed CompositeFont

--- MMA3/Source/MMA3/Widgets/Components/CustomTextBlock.cpp
+++ MMA3/Source/MMA3/Widgets/Components/CustomTextBlock.cpp
@@ -6,8 +6,6 @@
 
 UCustomTextBlock::UCustomTextBlock() {
 	ConstructorHelpers::FObjectFinder<UFont> l_Font(TEXT("Font'/Game/Assets/Fonts/Teko/F_Teko.F_Teko'"));
-	FCompositeFont l_CompositeFont = l_Font.Object->CompositeFont;
-	//Font.CompositeFont = TSharedPtr<FCompositeFont>(&l_CompositeFont);
 	Font.FontObject = l_Font.Object;
 	Font.TypefaceFontName = FName("Medium");
 }
